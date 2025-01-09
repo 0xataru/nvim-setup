aef4e28ec3e4e1337c76c970c92c0b3f87805a3c
eng1neeer_93 <raitermark@proton.me>
2025-01-09 10:01:50 +0200

    added rosepine colorscheme

--- lua/ataru/plugins/colorscheme.lua
+++ lua/ataru/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- load the colorscheme here
-    config = function()
-      vim.cmd([[
+	{
+		"projekt0n/github-nvim-theme",
+		-- "rose-pine/neovim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- load the colorscheme here
+		config = function()
+			vim.cmd([[
         colorscheme github_dark_default
+        "colorscheme rose-pine
         highlight Normal guibg=NONE
         highlight NonText guibg=NONE
       ]])
-    end,
-  },
+		end,
+	},
 }
