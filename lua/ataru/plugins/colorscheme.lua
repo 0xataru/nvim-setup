54b73db6e92ef6cebfc283a53cd2d356fd79bff1
ataru <0xataru@proton.me>
2025-01-27 11:18:02 +0200

    update 27/01/2025

--- lua/ataru/plugins/colorscheme.lua
+++ lua/ataru/plugins/colorscheme.lua
@@ -1,15 +1,34 @@
 return {
 	{
 		"projekt0n/github-nvim-theme",
-		-- "rose-pine/neovim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		-- load the colorscheme here
+		priority = 1000,
 		config = function()
+			vim.cmd("colorscheme github_dark_default")
+
 			vim.cmd([[
-        colorscheme github_dark_default
-        "colorscheme rose-pine
-        highlight Normal guibg=NONE
-        highlight NonText guibg=NONE
+        highlight Normal guibg=NONE ctermbg=NONE
+        highlight NonText guibg=NONE ctermbg=NONE
+
+        highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+        highlight NeoTreeFloatNormal guibg=NONE ctermbg=NONE
+        highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+        highlight NeoTreeCursorLine guibg=NONE ctermbg=NONE
+
+        highlight VertSplit guibg=NONE ctermbg=NONE
+        highlight SignColumn guibg=NONE ctermbg=NONE
+        highlight EndOfBuffer guibg=NONE ctermbg=NONE
+        highlight LineNr guibg=NONE ctermbg=NONE
+        highlight CursorLineNr guibg=NONE ctermbg=NONE
+        highlight Pmenu guibg=NONE ctermbg=NONE
+        highlight PmenuSel guibg=NONE ctermbg=NONE
+        highlight FloatBorder guibg=NONE ctermbg=NONE
+        highlight NormalFloat guibg=NONE ctermbg=NONE
+        highlight StatusLine guibg=NONE ctermbg=NONE
+        highlight StatusLineNC guibg=NONE ctermbg=NONE
+        highlight TabLine guibg=NONE ctermbg=NONE
+        highlight TabLineFill guibg=NONE ctermbg=NONE
+        highlight TabLineSel guibg=NONE ctermbg=NONE
+        highlight WinSeparator guibg=NONE ctermbg=NONE
       ]])
 		end,
 	},
